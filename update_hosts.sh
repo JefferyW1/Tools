d61927ed7ebea38b4c88e5c0cac76e22195ab306
JefferyW1 <90969070+JefferyW1@users.noreply.github.com>
2024-02-09 19:11:46 +0800

    Update update_hosts.sh

--- update_hosts.sh
+++ update_hosts.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-# 目标域名
+# 指定域名
 target_domain="hkg12s28-in-f3.1e100.net"
-# 本机hosts文件路径
-hosts_file="/etc/hosts"
-# 映射的域名
-mapped_domain="www.gstatic.com"
 
-# 获取目标域名的IP地址
-ip_address=$(dig +short "$target_domain")
+# 获取域名的IP地址
+ip_address=$(dig +short $target_domain)
 
-# 检查是否成功获取到IP地址
-if [ -n "$ip_address" ]; then
-    # 检查是否已经存在映射关系，如果存在则删除旧的映射
-    existing_mapping=$(grep -E "\s+$mapped_domain\s*$" "$hosts_file")
-    if [ -n "$existing_mapping" ]; then
-        sudo sed -i "/\s$target_domain\s*/d" "$hosts_file"
-    fi
-
-    # 添加新的映射
-    sudo bash -c "echo '$ip_address    $mapped_domain' >> $hosts_file"
-    echo "Hosts文件已更新，$target_domain 的IP地址映射到 $mapped_domain"
-else
-    echo "无法获取 $target_domain 的IP地址"
+# 如果没有找到IP地址，输出错误信息并退出
+if [ -z "$ip_address" ]; then
+  echo "无法获取 $target_domain 的IP地址。"
+  exit 1
 fi
+
+# 添加IP地址和对应的主机名到hosts文件的最上层位置
+echo "$ip_address    www.gstatic.com" | sudo tee -a /etc/hosts > /dev/null
+
+echo "成功将 $ip_address 添加到 /etc/hosts 文件的最上层位置。"
