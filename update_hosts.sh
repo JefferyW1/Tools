8d7531ad1f0f670d2bada06adf9a6c9a2c7cc916
JefferyW1 <90969070+JefferyW1@users.noreply.github.com>
2024-02-09 19:21:10 +0800

    Update update_hosts.sh

--- update_hosts.sh
+++ update_hosts.sh
@@ -15,11 +15,14 @@ fi
 # 临时文件，用于存储新的 hosts 文件内容
 temp_file=$(mktemp)
 
-# 在临时文件中插入新的IP地址和对应的主机名
-echo "$ip_address    www.gstatic.com" | cat - /etc/hosts > "$temp_file"
+# 创建新的 hosts 文件内容
+new_line="$ip_address    www.gstatic.com"
 
-# 将临时文件内容覆盖到 /etc/hosts 文件
-sudo cp "$temp_file" /etc/hosts
+# 检查是否已存在相同的行，如果存在则删除
+grep -v "^$new_line" /etc/hosts > "$temp_file"
+
+# 将新的IP地址和对应的主机名插入到文件的第一行
+echo "$new_line" | cat - "$temp_file" > /etc/hosts
 
 # 删除临时文件
 rm "$temp_file"
